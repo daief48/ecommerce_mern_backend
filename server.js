a27e0400b2bcf89ac021653787b615f0dda16586
daief48 <105385149+daief48@users.noreply.github.com>
2023-06-16 00:57:42 +0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ import cors from "cors";
 
 // configure env
 dotenv.config();
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 //database config
 connectDB();
 
