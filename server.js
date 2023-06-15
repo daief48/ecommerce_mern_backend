2f0fa5120538e2f7112a5686951511b996ac196c
daief48 <105385149+daief48@users.noreply.github.com>
2023-06-16 01:04:40 +0600

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,7 @@ import cors from "cors";
 
 // configure env
 dotenv.config();
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 //database config
 connectDB();
 
@@ -26,6 +21,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // rest api
 
