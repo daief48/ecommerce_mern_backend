7c39392b2220a9eaf697dc2a2572deb5cf006c65
daief48 <105385149+daief48@users.noreply.github.com>
2023-08-03 15:47:00 +0600

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+     phone2: {
+        type: String,
+        required: true
+    },
     address: {
         type: String,
         required: true
